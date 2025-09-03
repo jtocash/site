2ef2063e03b28a3e1692322dc69b3c68d57c0d06
Jack <jacktocash@icloud.com>
2025-09-03 02:54:22 -0400

    blocking

---
+++ my-app/src/MainView.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './MainView.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <style>
+
+</style>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', height: '75%', width: '75%', borderRadius: '0px'}}>
+          <p className='jost-test'> TEST </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
