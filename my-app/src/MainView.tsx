9149c33c0631131ebdccb0f6f3e82c2298f60f83
Jack <jacktocash@icloud.com>
2025-09-05 02:17:48 -0400

    improved font and added list

--- my-app/src/MainView.tsx
+++ my-app/src/MainView.tsx
@@ -2,22 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './MainView.css'
+import {Menu} from './Menu.tsx'
 
 function App() {
  
 
   return (
     <>
-    <style>
-@import url('https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap');
+   <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
 </style>
-
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-      
-          
-          <p className='instrument-sans-header'> TEST </p>
-        
+      <div style={{display: 'flex', flexDirection: 'column', width: 'fit-content'}}>
+        <Menu></Menu>
       </div>
+        
+    
     </>
   )
 }
---
+++ my-app/src/Menu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './MainView.css'
+
+export const Menu = () => {
+  return (
+    <>
+        <span className="header lato-regular"> projects</span>
+        <span className="header lato-regular"> bio</span>
+        <span className="header lato-regular"> test</span>
+        <span className="header lato-regular"> news</span>
+        <span className="header lato-regular"> blog</span>
+        <span className="header lato-regular"> test again</span>
+      
+      
+    </>
+  );
+};
